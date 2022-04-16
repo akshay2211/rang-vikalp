5c241dfa08db05142604b89feeb90bea54f9985d
Akshay Sharma <fxn769@gmail.com>
2022-04-16 14:23:31 +0530

    Sample Updated

--- app/src/main/java/io/ak1/rangvikalpsample/MainActivity.kt
+++ app/src/main/java/io/ak1/rangvikalpsample/MainActivity.kt
@@ -1,30 +1,22 @@
 package io.ak1.rangvikalpsample
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.statusBarsPadding
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import io.ak1.rangvikalp.RangVikalp
-import io.ak1.rangvikalp.colorArray
+import io.ak1.rangvikalpsample.ui.components.RangVikalpComposable
 import io.ak1.rangvikalpsample.ui.theme.RangVikalpSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,7 +39,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colors.background
                     ) {
-                        Demo()
+                        RangVikalpComposable()
                     }
                 }
             }
@@ -56,101 +48,13 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun Demo() {
-    LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-    var isVisible by remember {
-        mutableStateOf(true)
-    }
-    var color by remember {
-        mutableStateOf(colorArray[0][5])
-    }
-    var minimalView by remember {
-        mutableStateOf(true)
-    }
-    var intensity by remember {
-        mutableStateOf(5)
-    }
-    val floatAnimateAsState by animateFloatAsState(
-        targetValue = if (isVisible) 0f else 180f
-    )
-    Scaffold(modifier = Modifier.statusBarsPadding(),
-        bottomBar = {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(onClick = { isVisible = !isVisible }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_chevron_up),
-                            contentDescription = "",
-                            tint = color,
-                            modifier = Modifier
-                                .padding(12.dp)
-                                .rotate(floatAnimateAsState)
-                        )
-                    }
-                }
-                RangVikalp(
-                    isVisible = isVisible,
-                    showShades = !minimalView
-                ) {
-                    color = it
-                }
-            }
-
-        }) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(6.dp)) {
-            Text(text = "Minimal View", modifier = Modifier.padding(end = 6.dp))
-            Switch(checked = minimalView, onCheckedChange = {
-                minimalView = !minimalView
-            }, colors = SwitchDefaults.colors(checkedThumbColor = color))
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 200.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            IconButton(onClick = { isVisible = !isVisible }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_color),
-                    contentDescription = "",
-                    tint = color,
-                    modifier = Modifier
-                        .size(200.dp)
-                        .padding(4.dp)
-                )
-            }
-        }
-
-    }
-}
-@Composable
-fun LockScreenOrientation(orientation: Int) {
-    val context = LocalContext.current
-    DisposableEffect(Unit) {
-        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
-        val originalOrientation = activity.requestedOrientation
-        activity.requestedOrientation = orientation
-        onDispose {
-            // restore original orientation when view disappears
-            activity.requestedOrientation = originalOrientation
-        }
-    }
-}
-
-fun Context.findActivity(): Activity? = when (this) {
-    is Activity -> this
-    is ContextWrapper -> baseContext.findActivity()
-    else -> null
-}
-
-@Preview(showBackground = true)
+@Preview("Rang Vikalp")
+@Preview("Rang Vikalp (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview("Rang Vikalp (big font)", fontScale = 1.5f)
+@Preview("Rang Vikalp (large screen)", device = Devices.PIXEL_C)
 @Composable
 fun DefaultPreview() {
     RangVikalpSampleTheme {
-        Demo()
+        RangVikalpComposable()
     }
 }
---
+++ app/src/main/java/io/ak1/rangvikalpsample/ui/components/Utils.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 akshay2211 (Akshay Sharma)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.ak1.rangvikalpsample.ui.components
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
+
+/**
+ * Created by akshay on 16/04/22
+ * https://ak1.io
+ */
+
+
+
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
