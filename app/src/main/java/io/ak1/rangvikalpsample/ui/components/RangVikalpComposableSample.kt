a35e2b5972f0f0e098d5c9c9394ed4e4de1fd0d7
Akshay Sharma <fxn769@gmail.com>
2022-04-25 23:34:36 +0530

    new sample added

--- app/src/main/java/io/ak1/rangvikalpsample/ui/components/RangVikalpComposableSample.kt
+++ app/src/main/java/io/ak1/rangvikalpsample/ui/components/RangVikalpComposableSample.kt
@@ -20,18 +20,25 @@ import android.content.pm.ActivityInfo
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.statusBarsPadding
 import io.ak1.rangvikalp.RangVikalp
 import io.ak1.rangvikalp.defaultSelectedColor
 import io.ak1.rangvikalpsample.R
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 /**
@@ -40,72 +47,105 @@ import kotlinx.coroutines.launch
  */
 
 @Composable
-fun RangVikalpComposable() {
+fun RangVikalpComposable(themeCallback: (Boolean) -> Unit) {
     LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
     val defaultColor = defaultSelectedColor
-    val color = remember { Animatable(defaultColor) }
+    var color = remember { Animatable(defaultColor) }
     val coroutine = rememberCoroutineScope()
-    var isVisible by remember {
-        mutableStateOf(true)
-    }
-    var minimalView by remember {
-        mutableStateOf(true)
-    }
-    val floatAnimateAsState by animateFloatAsState(
-        targetValue = if (isVisible) 180f else 0f
-    )
-    Scaffold(modifier = Modifier.statusBarsPadding(),
-        bottomBar = {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(onClick = { isVisible = !isVisible }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_chevron_up),
-                            contentDescription = "",
-                            tint = color.value,
-                            modifier = Modifier
-                                .padding(12.dp)
-                                .rotate(floatAnimateAsState)
-                        )
-                    }
+    var isVisible by remember { mutableStateOf(false) }
+    var isShadesVisible by remember { mutableStateOf(false) }
+    var isLight by remember { mutableStateOf(true) }
+    val floatAnimateAsState by animateFloatAsState(targetValue = if (isVisible) 180f else 0f)
+
+    Scaffold(
+        topBar = {
+            Column(
+                modifier = Modifier
+                    .background(color.value)
+                    .statusBarsPadding()
+            ) {
+                TopAppBar(
+                    backgroundColor = Color.Transparent,
+                    elevation = 0.dp,
+
+                    ) {
+                    Text(
+                        text = "RangVikalp", modifier = Modifier.padding(16.dp, 0.dp)
+                    )
                 }
-                RangVikalp(
-                    isVisible = isVisible,
-                    showShades = !minimalView
-                ) {
-                    coroutine.launch {
-                        color.animateTo(it, animationSpec = tween(1000))
+            }
+
+        },
+        bottomBar = {
+            Surface(
+                color = MaterialTheme.colors.background
+            ) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(onClick = { isVisible = !isVisible }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_chevron_up),
+                                contentDescription = "",
+                                tint = color.value,
+                                modifier = Modifier
+                                    .padding(12.dp)
+                                    .rotate(floatAnimateAsState)
+                            )
+                        }
+                    }
+                    RangVikalp(
+                        isVisible = isVisible,
+                        showShades = isShadesVisible
+                    ) {
+                        coroutine.launch {
+                            async { color.animateTo(it, animationSpec = tween(1000)) }
+                        }
                     }
                 }
             }
 
         }) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(6.dp)) {
-            Text(text = "Minimal View", modifier = Modifier.padding(6.dp))
-            Switch(checked = minimalView, onCheckedChange = {
-                minimalView = !minimalView
-            }, colors = SwitchDefaults.colors(checkedThumbColor = color.value))
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 200.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            IconButton(onClick = { isVisible = !isVisible }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_color),
-                    contentDescription = "",
-                    tint = color.value,
-                    modifier = Modifier
-                        .size(200.dp)
-                        .padding(4.dp)
-                )
+
+        val modifier = Modifier.fillMaxWidth().padding(0.dp, 6.dp)
+        val roundedCornerShape = RoundedCornerShape(8.dp)
+
+        LazyColumn(Modifier.padding(16.dp, 0.dp)) {
+            item { Spacer(modifier = Modifier.height(8.dp)) }
+            items(
+                data(
+                    isVisible,
+                    isShadesVisible,
+                    isLight,
+                    { isVisible = !isVisible },
+                    { isShadesVisible = !isShadesVisible }) {
+                    isLight = !isLight
+                    themeCallback(isLight)
+                }) {
+                Row(
+                    modifier = modifier,
+                    horizontalArrangement = it.horizontalArrangement,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (it.horizontalArrangement == Arrangement.End) {
+                        Spacer(modifier = Modifier.width(70.dp))
+                    }
+                    Surface(
+                        color = color.value,
+                        shape = roundedCornerShape,
+                    ) {
+                        ClickableText(
+                            text = textFormatter(it.text),
+                            style = MaterialTheme.typography.body2.copy(color = LocalContentColor.current),
+                            modifier = Modifier.padding(8.dp), onClick = it.onClick
+                        )
+                    }
+                }
             }
         }
-
     }
 }
+
+
